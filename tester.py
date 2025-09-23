7eaf2944741c0eb13b8e651650cf9bb539b3a414
bianchini <felipe.bianchini@ucr.ac.cr>
2025-09-22 22:23:38 -0600

    Updated the tester to include and execute the parser

--- tester.py
+++ tester.py
@@ -1,11 +1,12 @@
 from src.lexer import Lexer
+from src.parser import Parser
 from src.utils import Error
 
 # Read file
-with open("tests/test_oficial.py", "r", encoding='utf-8') as f:
+with open("tests/test1.py", "r", encoding='utf-8') as f:
     source_code = f.read()
 
-# Create lexer
+# Create Lexer
 errors = [] # An empty error queue is created and sent to the lexer
 lexer = Lexer(errors=errors)
 lexer.build()
@@ -18,10 +19,15 @@ while True:
         break # End while when the lexer returns a null
     val = str(token.value)
     val = val.replace("\n", " \\n ") # \n replaced for reading purposes
-    print(f"{token.type}: {val} (line {token.lineno})")
+    #print(f"{token.type}: {val} (line {token.lineno})")
 
 # Check for errors
 if errors:
     print("Lexical errors found:")
     for error in errors:
         print(error)
+
+# Create Parser
+parser = Parser()
+result = parser.parser.parse(source_code, lexer=lexer.lex)
+print(result)
