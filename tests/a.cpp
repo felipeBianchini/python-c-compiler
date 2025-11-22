3e699d2e2ed551fdf30c3eedae3e5025cefe7b8b
bianchini <felipe.bianchini@ucr.ac.cr>
2025-11-22 15:04:31 -0600

    Modified tests

--- tests/a.cpp
+++ tests/a.cpp
@@ -1,9 +1,21 @@
 #include <any>
+#include <cmath>
+# include <iostream>
 
-std::any fibonacci(std::any n)
-{
-    int a = 2;
-    bool b = true;
-    std::any c = 2 + b + a;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+auto fib(std::any n) {
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < 3; i++)
+     {
+        float a = 2 + b;
+        float b = a + 1;
+        int n = std::floor(a / b);
+
+    }
+
+    return a;
+}
+
+int main() {
+    return 0;
 }
