715b30467eff0f5a4c3946a329338157a76e5775
andre.salaschinchilla <andre.salaschinchilla@ucr.ac.cr>
2025-11-22 22:48:08 -0600

    Change to the testing .py

--- tests/a.py
+++ tests/a.py
@@ -1,18 +1,10 @@
-c = 0
-def fib(n):
-    a = 0
-    b = 1
-    c = 0
+def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
-    for i in range(n):
-        c = a + b
-        b = a
-        a = c
-    return a
+    res = fibonacci(n - 1) + fibonacci(n - 2)
+    return res
 
-c = 1 + 2
 for i in range(10):
-    print(fib(i))
+    print(fibonacci(i))
